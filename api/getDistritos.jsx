073b5fd6acb9bf07f0939d3f109293da33a5a2df
scsx <4182349+scsx@users.noreply.github.com>
2024-01-13 02:37:49 +0000

    Deletes

--- api/getDistritos.jsx
+++
@@ -1,9 +0,0 @@
-const getDistritos = () => {
-  return fetch('https://json.geoapi.pt/distritos')
-    .then((res) => res.json())
-    .catch((error) => {
-      throw error
-    })
-}
-
-export default getDistritos
--- api/getFreguesia.jsx
+++
@@ -1,9 +0,0 @@
-const getFreguesia = (freguesia) => {
-  return fetch(`https://json.geoapi.pt/freguesia/${freguesia}`)
-    .then((res) => res.json())
-    .catch((error) => {
-      throw error
-    })
-}
-
-export default getFreguesia
