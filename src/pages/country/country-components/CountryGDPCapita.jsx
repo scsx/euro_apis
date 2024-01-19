d437a5bbe95c707e16a1c705051257989e45d896
scsx <4182349+scsx@users.noreply.github.com>
2024-01-19 02:16:13 +0000

    Graph stable

--- src/pages/country/country-components/CountryGDPCapita.jsx
+++ src/pages/country/country-components/CountryGDPCapita.jsx
@@ -1,7 +1,7 @@
 import * as d3 from 'd3'
 import { useEffect, useState, useRef } from 'react'
 
-//import data from '../../../data/other/unece/Population-density.json'
+import GDPdata from '../../../data/other/unece/Population-density.json'
 import Alert from 'react-bootstrap/Alert'
 import Loading from '../../../components/Loading'
 
@@ -9,60 +9,87 @@ const CountryGDPCapita = ({ cca3 }) => {
   const [loading, setLoading] = useState(true)
   const [noDataMsg, setNoDataMsg] = useState('')
 
+  console.log(GDPdata)
+
   const Barchart = () => {
-    const ref = useRef()
+    const graphcontainer = useRef()
+    const refSVG = useRef()
 
+    const data = [
+      {
+        Country: 'United States',
+        Value: '12394'
+      },
+      {
+        Country: 'Russia',
+        Value: '6148'
+      },
+      {
+        Country: 'Germany (FRG)',
+        Value: '1653'
+      },
+      {
+        Country: 'France',
+        Value: '2162'
+      }
+    ]
     useEffect(() => {
-      // set the dimensions and margins of the graph
-      const margin = { top: 30, right: 30, bottom: 70, left: 60 },
-        width = 1460 - margin.left - margin.right,
-        height = 800 - margin.top - margin.bottom
+      // Set the dimensions and geometrys of the graph
+      const geometry = { top: 30, right: 30, bottom: 100, left: 60 },
+        width = 1296 - geometry.left - geometry.right,
+        height = 600 - geometry.top - geometry.bottom
 
-      // append the svg object to the body of the page
+      // Append the svg object to the body of the page
       const svg = d3
-        .select(ref.current)
+        .select(refSVG.current)
         .append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
+        .attr('width', width + geometry.left + geometry.right)
+        .attr('height', height + geometry.top + geometry.bottom)
         .append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`)
+        .attr('transform', `translate(${geometry.left},${geometry.top})`)
 
-      // Parse the Data
-      d3.csv(
-        'https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv'
-      ).then(function (data) {
-        // X axis
-        const x = d3
-          .scaleBand()
-          .range([0, width])
-          .domain(data.map((d) => d.Country))
-          .padding(0.2)
-        svg
-          .append('g')
-          .attr('transform', `translate(0, ${height})`)
-          .call(d3.axisBottom(x))
-          .selectAll('text')
-          .attr('transform', 'translate(-10,0)rotate(-45)')
-          .style('text-anchor', 'end')
+      // X axis
+      const x = d3
+        .scaleBand()
+        .range([0, width])
+        .domain(data.map((d) => d.Country))
+        .padding(0.2)
 
-        // Add Y axis
-        const y = d3.scaleLinear().domain([0, 13000]).range([height, 0])
-        svg.append('g').call(d3.axisLeft(y))
+      // Y axis
+      const y = d3.scaleLinear().domain([0, 13000]).range([height, 0])
+      svg.append('g').call(d3.axisLeft(y)).style('font-size', '14px')
 
-        // Bars
-        svg
-          .selectAll('mybar')
-          .data(data)
-          .join('rect')
-          .attr('x', (d) => x(d.Country))
-          .attr('y', (d) => y(d.Value))
-          .attr('width', x.bandwidth())
-          .attr('height', (d) => height - y(d.Value))
-          .attr('fill', '#5f0f40')
-      })
+      svg
+        .append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(
+          d3
+            .axisBottom(x)
+            .tickSize(10) // Adjust the size of ticks
+            .tickPadding(10) // Adjust the padding between ticks and labels
+        )
+        .selectAll('text')
+        .attr('transform', 'translate(-10,0)rotate(-45)')
+        .style('text-anchor', 'end')
+        .style('font-size', '14px')
+
+      // Bars
+      svg
+        .selectAll('mybar')
+        .data(data)
+        .join('rect')
+        .attr('x', (d) => x(d.Country))
+        .attr('y', (d) => y(d.Value))
+        .attr('width', x.bandwidth())
+        .attr('height', (d) => height - y(d.Value))
+        .attr('fill', '#5f0f40')
     }, [])
 
-    return <svg width={1460} height={800} id='barchart' ref={ref} />
+    return (
+      <div className='graphbox' ref={graphcontainer}>
+        <svg width={1460} height={800} id='barchart' ref={refSVG} />
+      </div>
+    )
   }
 
   return <Barchart />
