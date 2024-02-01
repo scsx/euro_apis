c3ffab3106350d54d3537787b7655bcbdd3694ef
scsx <4182349+scsx@users.noreply.github.com>
2024-02-01 19:04:59 +0000

    TextWithImage

--- src/components/blocks/TextWithImage.jsx
+++ src/components/blocks/TextWithImage.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 import './Blocks.scss'
 
-const OneThirdTextTwoThirdsImage = ({
+const TextWithImage = ({
   title, // Château d'If, Frioul
   text,
   bgcolorText = 'white',
   bgcolorImage,
   imageOnRight = true,
+  imageIsTwoThirds,
   image,
   extrastyles,
   extraclasses
@@ -24,22 +25,42 @@ const OneThirdTextTwoThirdsImage = ({
     backgroundColor: bgcolorText || undefined
   }
 
+  // imageIsTwoThirds: true for yes, false for text being two thirds, undefined for 50% each
+  let textColWidth, imageColWidth
+
+  if (imageIsTwoThirds === undefined) {
+    textColWidth = 'col-md-6'
+    imageColWidth = 'col-md-6'
+  } else if (imageIsTwoThirds) {
+    textColWidth = 'col-md-4'
+    imageColWidth = 'col-md-8'
+  } else {
+    textColWidth = 'col-md-8'
+    imageColWidth = 'col-md-4'
+  }
+
   return (
     <section
-      className={`block block1text2image ${extraclasses}`}
+      className={`block textwithimage ${extraclasses}`}
       style={inlineStyle}>
       <div className='row'>
-        <div className={`col-md-4 block1text2image__texthalf ${!imageOnRight ? 'order-2' : ''}`} style={textHalfStyle}>
-          <div className='block1text2image__textcontent'>
+        <div
+          className={`textwithimage__texthalf ${textColWidth} ${
+            !imageOnRight ? 'order-2' : ''
+          }`}
+          style={textHalfStyle}>
+          <div className='textwithimage__textcontent'>
             <h2 className='block__title'>{title}</h2>
-            <div className='block__text'>{text}</div>
+            <div className='block__text textwithimage__text'>{text}</div>
           </div>
         </div>
         <div
-          className={`col-md-8 block1text2image__imagehalf ${!imageOnRight ? 'order-1' : ''}`}
+          className={`textwithimage__imagehalf ${imageColWidth} ${
+            !imageOnRight ? 'order-1' : ''
+          }`}
           style={imageHalfStyle}>
           <img
-            className='block__image block1text2image__image'
+            className='block__image textwithimage__image'
             src={`./public/deco/${image}`}
             alt={title}
           />
@@ -49,15 +70,16 @@ const OneThirdTextTwoThirdsImage = ({
   )
 }
 
-OneThirdTextTwoThirdsImage.propTypes = {
+TextWithImage.propTypes = {
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   bgcolorText: PropTypes.string,
   bgcolorImage: PropTypes.string.isRequired,
   imageOnRight: PropTypes.bool,
+  imageIsTwoThirds: PropTypes.bool,
   image: PropTypes.string,
   extrastyles: PropTypes.object,
   extraclasses: PropTypes.string
 }
 
-export default OneThirdTextTwoThirdsImage
+export default TextWithImage
--- src/pages/AllBlocks.jsx
+++ src/pages/AllBlocks.jsx
@@ -1,11 +1,17 @@
 import SingleText from '../components/blocks/singleText'
-import OneThirdTextTwoThirdsImage from '../components/blocks/OneThirdTextTwoThirdsImage'
+import TextWithImage from '../components/blocks/TextWithImage'
 
 let singleParagraph =
   'The single line component is used to hold basic text strings like the name of an item or title of an article. When adding this component to a shape, specify a name and description for the sort of content that you wish for it to contain.'
 
 let chateauParagraph =
   'One nautical mile from the Old Port of Marseille, you can make out the silhouette of the Château d’If and the Frioul archipelago. This monument, perched on a small limestone rock in the middle of the Mediterranean waters, became famous thanks to the novel The Count of Monte Cristo by Alexandre Dumas. More than a prison, the Château d’If was also a fortress designed to protect the city from enemy invasions and from itself.'
+
+let arlesParagraph =
+  'Built in the first century A.D., the Arles Amphitheater helped to make the city one of the largest Roman metropolises in Gaul. The arenas have survived for more than two millennia in an excellent state of preservation, resisting wars, epidemics, and the temptation of men to use them as stone quarries. As one of the best-preserved ancient amphitheaters in Europe, they can be visited all year round and continue to host cultural and sporting events, perpetuating their original function.'
+
+let abbeyParagraph =
+  'The Montmajour Abbey is an exceptional monument that bears witness to eight centuries of history and architecture in Provence.'
 
 let singleTextStyles = {
   backgroundPosition: 'top center',
@@ -20,7 +26,14 @@ const AllBlocks = () => {
         title={'Single Text'}
         text={singleParagraph}
         extrastyles={singleTextStyles}
-        extraclasses='block--bluegreecetop'
+      />
+      <TextWithImage
+        title={'Montmajour Abbey'}
+        text={abbeyParagraph}
+        image={'abbey.jpg'}
+        bgcolorText={'#d6dccd'}
+        bgcolorImage={'#fefaf2'}
+        extraclasses='abbey'
       />
       <SingleText
         title={'Another Single Text'}
@@ -28,13 +41,23 @@ const AllBlocks = () => {
         bgimage={'greece.jpg'}
         extrastyles={singleTextStyles}
       />
-      <OneThirdTextTwoThirdsImage
+      <TextWithImage
         title={"Château d'If, Frioul archipelago, Marseille, 16th century"}
         text={chateauParagraph}
         image={'chateau-dif.jpg'}
         bgcolorText={'#fefaf2'}
         bgcolorImage={'#54709b'}
+        imageIsTwoThirds={false}
+      />
+      <TextWithImage
+        title={'Arles Amphitheater'}
+        text={arlesParagraph}
+        image={'arles.jpg'}
+        bgcolorText={'#18184f'}
+        bgcolorImage={'#244372'}
+        imageIsTwoThirds={false}
         imageOnRight={false}
+        extraclasses='arles'
       />
     </>
   )
