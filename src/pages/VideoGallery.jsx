ed05984aabf6fcea14b7f2bb263a9defab9f5f2a
scsx <4182349+scsx@users.noreply.github.com>
2024-02-03 05:02:09 +0000

    Videos playing, running methods and getting current time

--- src/pages/VideoGallery.jsx
+++ src/pages/VideoGallery.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import { useRef } from 'react'
+import ReactPlayer from 'react-player'
 
 const VideoGallery = () => {
+  const video1 = useRef()
+
+  const videoPaused = () => {
+    console.log('Video paused -> save to redux')
+    console.log(video1.current.getCurrentTime())
+  }
+
+  const videoStopped = () => {
+    console.log('Video stopped -> save to redux')
+  }
+
   return (
-    <div>VideoGallery</div>
+    <div className='row'>
+      <div className='col-md-4'>
+        {' '}
+        <ReactPlayer
+          url='/videos/atlantiques.mp4'
+          controls
+          width='100%'
+          height='auto'
+          muted={true}
+          onPause={videoPaused}
+          onEnded={videoStopped}
+          ref={video1}
+        />
+      </div>
+      <div className='col-md-4'>
+        <ReactPlayer
+          url='/videos/farewell-to-europe.mp4'
+          controls
+          width='100%'
+          height='auto'
+          muted={true}
+          onPause={videoPaused}
+          onEnded={videoStopped}
+        />
+      </div>
+      <div className='col-md-4'>
+        <ReactPlayer
+          url='/videos/ossessione_1943.mp4'
+          controls
+          width='100%'
+          height='auto'
+          muted={true}
+          onPause={videoPaused}
+          onEnded={videoStopped}
+        />
+      </div>
+    </div>
   )
 }
 
