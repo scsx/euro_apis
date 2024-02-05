cd0e94631c79419bc02c7cc6127172efc14f0d71
scsx <4182349+scsx@users.noreply.github.com>
2024-02-05 18:13:42 +0000

    install and first config

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+import videosReducer from './reducers/videosReducer'
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, videosReducer)
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  persistedReducer
 })
 
-export default store
+const persistor = persistStore(store)
+
+export { store, persistor }
