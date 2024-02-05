a45ddb27fb1e4d5296cb5339f39345bd16fe4c05
scsx <4182349+scsx@users.noreply.github.com>
2024-02-05 18:27:41 +0000

    Guardar state em localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers'
 
+// Load state from localStorage
+const persistedState = JSON.parse(localStorage.getItem('reduxState')) || {}
+
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  preloadedState: persistedState // Provide preloadedState during store creation
+})
+
+// Subscribe to store updates and save state to localStorage
+store.subscribe(() => {
+  const stateToPersist = store.getState()
+  localStorage.setItem('reduxState', JSON.stringify(stateToPersist))
 })
 
 export default store
